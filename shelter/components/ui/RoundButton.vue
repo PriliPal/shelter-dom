8bd1160346e101667bb995fae6b82d7ec302b820
ILYSHENKA <prilipal17@gmail.com>
2023-09-06 21:32:59 +0300

    init commit

---
+++ shelter/components/ui/RoundButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+      class="cursor-pointer unselectable"
+      :class="isActive ? 'button-round-active' : 'button-round-inactive'"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import '../../assets/scss/buttons.scss'
+import '../../assets/scss/helpers.scss'
+
+const props = defineProps({
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+})
+
+</script>
